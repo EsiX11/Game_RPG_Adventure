71e7f18d38fa66889ec4c5a87c302d446cb991e2
EsiX11 <32106733+EsiX11@users.noreply.github.com>
2019-08-01 21:02:58 +0200

    Update button into box

--- Game_RPG_Adventure/js/main.js
+++ Game_RPG_Adventure/js/main.js
@@ -7,12 +7,33 @@
         x.style.display = "none";
     }
 }
+function hideButtonNext() {
+    var x = document.getElementById("buttonNext");
+    if (x.style.display === "none") {
+        x.style.display = "inline-block";
+    }
+    else {
+        x.style.display = "none";
+    }
+}
+function hideButtonBack() { 
+    var x = document.getElementById("buttonBack");
+    if (x.style.display === "none") {
+        x.style.display = "inline-block";
+    }
+    else {
+        x.style.display = "none";
+    }
+}
 var storyNumber = -1;
 var headerNumber = 0;
 function story(number, headerNumber) {
     if (number < 0){
         storyNumber = 0;
         number = 0;
+    }
+    if (number === 1){
+        hideButtonBack();
     }
     var url = "https://esix11.github.io/Game_RPG_Adventure/Game_RPG_Adventure/json/story.json";
     const jFile = new XMLHttpRequest();
@@ -25,6 +46,7 @@ function story(number, headerNumber) {
         var storyHeader = json[json.header+headerNumber];
         if (number >= storyHeader.length){
             number = storyHeader.length - 1;
+            hideButtonNext();
             console.log("Uuhm")
         }
         storyBox.innerHTML = storyHeader[number];
